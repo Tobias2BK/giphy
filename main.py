bc4aca6c7e570cc51664b044511fb0d90da15096
Tobi Mitchell <tcent9580@gmail.com>
2025-06-12 10:26:14 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ def track():
     ip = request.headers.get('X-Forwarded-For',request.remote_addr)
     time = datetime.utcnow().isoformat()
     print(f'Time: {time} - Ip: {ip}')
-    return send_file('test.gif',minetype='image/gif')
+    return send_file('test.gif',mimetype='image/gif')
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5000)
