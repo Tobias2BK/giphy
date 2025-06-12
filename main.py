1c154598a5f456112972cf567d0470e87232e444
Tobi Mitchell <tcent9580@gmail.com>
2025-06-12 10:05:56 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import *
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route('/test.gif')
+def track():
+    ip = request.headers.get('X-Forwarded-For',request.remote_addr)
+    time = datetime.utcnow().isoformat()
+    print(f'Time: {time} - Ip: {ip}')
+    return send_file('pixel.gif',minetype='image/gif')
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=5000)
